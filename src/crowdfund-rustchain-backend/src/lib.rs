58a8ab0eb76273067e42a8ad95bc409cb4f313fc
Bestine <bestineokinda@gmail.com>
2025-08-06 11:33:02 +0300

    Resolved ic-stable-structure version to earlier one

--- src/crowdfund-rustchain-backend/src/lib.rs
+++ src/crowdfund-rustchain-backend/src/lib.rs
@@ -1,4 +1,37 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+// #[ic_cdk::query]
+// fn greet(name: String) -> String {
+//     format!("Hello, {}!", name)
+// }
+
+// ------------IMPORTS-------------
+use candid::{CandidType, Decode, Deserialize, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
+use std::{borrow::Cow, cell::RefCell};
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+const MAX_VALUE_SIZE: u32 = 100;
+
+#[derive(CandidType, Deserialize)] // Define Macros for exam struct
+struct Exam {
+    out_of: u8,
+    course: String,
+    curve: u8,
+}
+
+// Define and implement trains for the Exam structs
+impl Storable for Exam {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl BoundedStorable for Exam {
+    const MAX_SIZE: u32 = MAX_VALUE_SIZE;
+    const IS_FIXED_SIZE: bool = false;
 }
