2fecda025343097562514886c0f0795c8f4a174a
Bestine <bestineokinda@gmail.com>
2025-08-06 11:57:27 +0300

    Define storage for dynamic storage

--- src/crowdfund-rustchain-backend/src/lib.rs
+++ src/crowdfund-rustchain-backend/src/lib.rs
@@ -35,3 +35,24 @@ impl BoundedStorable for Exam {
     const MAX_SIZE: u32 = MAX_VALUE_SIZE;
     const IS_FIXED_SIZE: bool = false;
 }
+
+//Define memory manager for dynamic storage
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
+        MemoryManager::init(
+            DefaultMemoryImpl::default(),
+        )
+    );
+
+    static EXMAP_MAP: RefCell<StableBTreeMap<u64, Exam, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)))
+        )
+    );
+
+    static PARTICIPATION_PERCENTAGE_MAP: RefCell<StableBTreeMap<u64, u64, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
+        )
+    );
+}
