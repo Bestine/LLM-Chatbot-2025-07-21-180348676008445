5012ee4d2b46afdcfe8ab82f70db9744bb7a304b
Bestine <bestineokinda@gmail.com>
2025-08-08 14:39:19 +0300

    Edit and create proposal implemented traits and defined limits for the proposal

--- src/crowdfund-rustchain-backend/src/lib.rs
+++ src/crowdfund-rustchain-backend/src/lib.rs
@@ -7,6 +7,7 @@ type Memory = VirtualMemory<DefaultMemoryImpl>;
 
 const MAX_VALUE_SIZE: u32 = 5000;
 
+// Define a proposal struct
 #[derive(CandidType, Deserialize)]
 struct Proposal {
     description: String,
@@ -16,4 +17,28 @@ struct Proposal {
     is_active: bool,
     voted: Vec<candid::Principal>,
     owner: candid::Principal,
+}
+
+// Create proposal struct for create and edit proposal
+#[derive(CandidType, Deserialize)]
+struct CreateProposal {
+    description: String,
+    is_active: bool,
+}
+
+// Implement traits for Proposal
+impl Storable for Proposal {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(&self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+//Define limits for Proposal
+impl BoundedStorable for Proposal {
+    const MAX_SIZE: u32 = MAX_VALUE_SIZE;
+    const IS_FIXED_SIZE: bool = false;
 }
